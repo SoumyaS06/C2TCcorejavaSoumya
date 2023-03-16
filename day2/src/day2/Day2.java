fec0f355375df98e0770a262a10311fd96e9f10c
CSELAB <CSELAB@DESKTOP-1JLDSA7>
2023-03-16 15:19:11 +0530

    first  commit

---
+++ day2/src/day2/Day2.java
@@ -0,0 +1,19 @@
+package day2;
+class Student{
+	String name="kavya";
+			int id=456;
+	        int score=90;
+	
+}
+
+public class Day2 {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+     Student s1=new Student();
+     System.out.println(s1.name);
+     System.out.println(s1.id);
+     System.out.println(s1.score);
+    		 }
+
+}
