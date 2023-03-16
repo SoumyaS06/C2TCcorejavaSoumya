06a817f27bfcfca7bab98b551a3e16108d3df820
CSELAB <CSELAB@DESKTOP-1JLDSA7>
2023-03-16 15:27:10 +0530

    weeks

---
+++ Ha/src/Ha.java
@@ -0,0 +1,28 @@
+
+public class Ha {
+
+	public static void main(String[] args) {
+		 int choice=1;
+		switch(choice) {
+		case 1:System.out.println("Monday");
+		break;
+		case 2:System.out.println("Tuesday");
+		break;
+		case 3:System.out.println("Wednesday");
+		break;
+		case 4:System.out.println("Thursday");
+		break;
+		case 5:System.out.println("Friday");
+		break;
+		case 6:System.out.println("Saturday");
+		break;
+		default:
+		System.out.println("Invalid");
+		
+		
+		
+		}
+
+	}
+
+}
